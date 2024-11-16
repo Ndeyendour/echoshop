ecec2e29bfbb4d1b31fe991b3bcabbab35cb2522
Ndeyendour <ndeyefatou023@gmail.com>
2024-11-16 22:52:38 +0000

    last commit

--- my-botanical-store-frontend/src/pages/ProductList.js
+++ my-botanical-store-frontend/src/pages/ProductList.js
@@ -39,7 +39,7 @@ function ProductList() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/products');
+        const response = await axios.get('https://my-botanical-store-backend.vercel.app/products');
         setProducts(response.data);
       } catch (error) {
         console.error('Error fetching products:', error);
